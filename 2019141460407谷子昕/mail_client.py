dec11f7851645091cfe207d9ff0e9adaf66bd6b9
naocanwang <68321629+naocanwang@users.noreply.github.com>
2021-06-25 22:33:39 +0800

    Create mail_client.py
    mail client 程序

---
+++ 2019141460407谷子昕/mail_client.py
@@ -0,0 +1,46 @@
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+
+# 第三方 SMTP 服务
+mail_host = "smtp.163.com"  # SMTP服务器
+mail_user = "naocanwang"  # 用户名
+mail_pass = "DWCKGLHHYQOFICDQ"  # 授权密码
+
+sender ='naocanwang@163.com'   # 发件人邮箱
+receivers = '736547619@qq.com'  # 接收邮件
+
+content = 'test'
+title = '测试'  # 邮件主题
+
+
+def sendEmail():
+    message = MIMEText(content, 'plain', 'utf-8')  # 内容, 格式, 编码
+    message['From'] = "{}".format(sender)
+    message['To'] = ",".join(receivers)
+    message['Subject'] = title
+
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host, 465)  # 启用SSL发信, 端口是465
+        smtpObj.login(mail_user, mail_pass)  # 登录验证
+        smtpObj.sendmail(sender, receivers, message.as_string())  # 发送
+        print("mail has been send successfully.")
+    except smtplib.SMTPException as e:
+        print(e)
+
+
+def send_email2(SMTP_host, from_account, from_passwd, to_account, subject, content):
+    email_client = smtplib.SMTP(SMTP_host)
+    email_client.login(from_account, from_passwd)
+    # create msg
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['Subject'] = Header(subject, 'utf-8')  # subject
+    msg['From'] = from_account
+    msg['To'] = to_account
+    email_client.sendmail(from_account, to_account, msg.as_string())
+
+    email_client.quit()
+
+
+if __name__ == '__main__':
+    sendEmail()
