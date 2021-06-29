c068b9e2f2639d363157222932193c8ca0ebd7a5
xiao-yi1 <54842187+xiao-yi1@users.noreply.github.com>
2021-06-29 22:55:57 +0800

    Update server.py

--- 2019141460490/server.py
+++ 2019141460490/server.py
@@ -1,142 +1,207 @@
+
+import json
 import pickle
 import socket
 import threading
-import queue
-import json  # json.dumps(some)打包   json.loads(some)解包
+import tkinter as tk
+import tkinter.messagebox
+from tkinter.scrolledtext import ScrolledText
 import config
 
+target_IP = config.target_IP
+port  = config.PORT
 
-Server_IP = config.Server_IP
-PORT = int(config.Server_Port)
+usr_name = ''
+usr_pwd = ''
+server_name = ''
 
-# Server_IP = '127.0.0.1'
-# PORT = 9999
 
-messages = queue.Queue()
-users = []   # 0:用户名 1:连接
-lock = threading.Lock()
+def usr_login(*args):
+    global usr_name
+    global usr_pwd
+    global server_name
+    usr_name = var_usr_name.get()
+    usr_pwd = var_usr_pwd.get()
+    server_name = var_server_name.get()
 
-def onlines():    # 统计当前在线人员
-    online = []
-    for i in range(len(users)):
-        online.append(users[i][0])
-    return online
+    # 发送访问请求,验证用户信息
+    s.send(usr_name.encode() + '~'.encode() + usr_pwd.encode())
 
-class ChatServer(threading.Thread):
-    global users,lock
+    comfirm = s.recv(1024).decode()
+    print(comfirm)
+    if (comfirm == '用户名不存在'):
+        usr_sign_up = tk.messagebox.askyesno(title='注册', message='欢迎！不过你还没有注册哦，需要注册吗？')
+        if usr_sign_up:
+            _usr_sign_up()
+    elif (comfirm == '密码错误'):
+        tk.messagebox.showerror('密码错误,请重新输入！')
 
-    def __init__(self):         # 构造函数
-        threading.Thread.__init__(self)
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+        window.destroy()
 
-    def receive(self, conn, addr):             # 接收消息
-        # ack判断验证是否通过
-        ack='0'
-        while(ack!='1'):
-            info = conn.recv(1024)        #接收用户登陆请求
-            info = info.decode()
-            # 处理注册
-            if(info[0:3]=='reg'):
-                with open('usrs_info.pickle', 'rb') as usr_file:
-                    usrs_info = pickle.load(usr_file)
-                    usr_file.close()
-                    _,usr_name,usr_pwd = info.split('~')
-                    # 判断是否重复注册
-                    if(usr_name in usrs_info):
-                        conn.send('用户已经注册'.encode())
-                    else:
-                        usrs_info.setdefault(usr_name,usr_pwd)
-                        with open('usrs_info.pickle', 'wb') as usr_file:
-                            pickle.dump(usrs_info,usr_file)
-                            usr_file.close()
-                        conn.send('注册成功！'.encode())
-                    continue
-            usr_name,usr_pwd = info.split('~')
-            print(usr_pwd+usr_name)
-            print(addr)
-            try:
-                with open('test/usrs_info.pickle', 'rb') as usr_file:
-                    usrs_info = pickle.load(usr_file)
-            except FileNotFoundError:
-                with open('test/usrs_info.pickle', 'wb') as usr_file:
-                    # 没有文件创建管理员
-                    usrs_info = {'admin': 'admin'}
-                    pickle.dump(usrs_info, usr_file)
-            if usr_name in usrs_info:
-                if usr_pwd == usrs_info[usr_name]:
-                    ack='1'
-                    conn.send(ack.encode())
-                    print(ack)
-                else:
-                    nock='密码错误'
-                    conn.send(nock.encode())
-            else:
-                nock = '用户名不存在'
-                conn.send(nock.encode())
-        users.append((usr_name, conn))
-        USERS = onlines()
-        self.Load(USERS,addr)
-        # 在获取用户名后便会不断地接受用户端发来的消息（即聊天内容），结束后关闭连接。
+
+#     进入聊天室
+
+
+def _usr_sign_up():
+    def sign_to_chatroom():
+        np = new_pwd.get()
+        npf = new_pwd_confirm.get()
+        nn = new_name.get()
+
+        if np != npf:
+            tk.messagebox.showerror(title='错误！', message='确认密码不一致，请重新输入！')
+        else:
+            register = 'reg~' + nn + '~' + np
+            s.send(register.encode())
+            data = s.recv(1024).decode()
+            if (data == '注册成功！'):
+                tk.messagebox.showinfo(title='恭喜', message='注册成功！')
+                window_sign_up.destroy()
+            if (data == '用户已经注册'):
+                tk.messagebox.showerror(title='错误！', message="用户已经注册")
+
+    window_sign_up = tk.Toplevel(window)
+    window_sign_up.geometry('350x200')
+    window_sign_up.title('注册窗口')
+
+    new_name = tk.StringVar()
+    new_name.set('张三')
+    tk.Label(window_sign_up, text='用户名').place(x=10, y=10)
+    entry_new_name = tk.Entry(window_sign_up, textvariable=new_name)
+    entry_new_name.place(x=150, y=10)
+
+    new_pwd = tk.StringVar()
+    tk.Label(window_sign_up, text='密码').place(x=10, y=50)
+    entry_usr_pwd = tk.Entry(window_sign_up, textvariable=new_pwd, show='*')
+    entry_usr_pwd.place(x=150, y=50)
+
+    new_pwd_confirm = tk.StringVar()
+    tk.Label(window_sign_up, text='确认密码').place(x=10, y=90)
+    entry_usr_pwd_confirm = tk.Entry(window_sign_up, textvariable=new_pwd_confirm, show='*')
+    entry_usr_pwd_confirm.place(x=150, y=90)
+
+    btn_comfirm_sign_up = tk.Button(window_sign_up, text='注册', command=sign_to_chatroom)
+    btn_comfirm_sign_up.place(x=150, y=130)
+
+
+window = tk.Tk()
+window.title('Welcome to my chatroom')
+window.geometry('450x300')
+window.resizable(0, 0)
+var_usr_name = tk.StringVar();
+var_usr_pwd = tk.StringVar();
+var_server_name = tk.StringVar();
+var_server_name.set(target_IP+':'+port)
+# var_usr_name.set('xy')
+# var_usr_pwd.set('123')
+# 登录界面:欢迎图片
+canvas = tk.Canvas(window, height=200, width=500)
+image_file = tk.PhotoImage(file='welcome.gif')
+image = canvas.create_image(0, 0, anchor='nw', image=image_file)
+canvas.pack(side='top')
+
+# 用户信息
+tk.Label(window, text='目标主机').place(x=50, y=130)
+tk.Label(window, text='用户名:').place(x=50, y=170)
+tk.Label(window, text='密码:').place(x=50, y=210)
+
+# 输入框
+entry_server_name = tk.Entry(window, textvariable=var_server_name)
+entry_server_name.place(x=160, y=130)
+
+entry_usr_name = tk.Entry(window, textvariable=var_usr_name)
+entry_usr_name.place(x=160, y=170)
+
+entry_usr_pwd = tk.Entry(window, textvariable=var_usr_pwd, show='*')
+entry_usr_pwd.place(x=160, y=210)
+
+btn_login = tk.Button(window, text='登录', command=usr_login)
+btn_login.place(x=170, y=250)
+window.bind('<Return>', usr_login)
+btn_sign_up = tk.Button(window, text='注册', command=_usr_sign_up)
+btn_sign_up.place(x=270, y=250)
+# 建立连接
+server_name = var_server_name.get()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_ip, server_port = server_name.split(':')
+s.connect((server_ip, int(server_port)))
+
+window.mainloop()
+
+# 聊天室：聊天窗口
+Chatroom = tkinter.Tk()
+Chatroom.geometry("640x480")
+Chatroom.title('聊天室')
+Chatroom.resizable(0, 0)
+
+# 消息界面
+listbox = ScrolledText(Chatroom)
+listbox.place(x=5, y=0, width=640, height=320)
+listbox.tag_config('tag1', foreground='red', backgroun="yellow")
+listbox.insert(tkinter.END, '欢迎进入群聊，大家开始聊天吧!', 'tag1')
+
+INPUT = tkinter.StringVar()
+INPUT.set('')
+entryIuput = tkinter.Entry(Chatroom, width=120, textvariable=INPUT)
+entryIuput.place(x=5, y=320, width=580, height=170)
+
+# 在线用户列表
+listbox1 = tkinter.Listbox(Chatroom)
+listbox1.place(x=510, y=0, width=130, height=320)
+
+
+def send(*args):
+    message = entryIuput.get() + '~' + usr_name + '~'
+    s.send(message.encode())
+    INPUT.set('')
+
+
+sendButton = tkinter.Button(Chatroom, text="\n发\n\n\n送", anchor='n', command=send, font=('Helvetica', 18), bg='white')
+sendButton.place(x=585, y=320, width=55, height=300)
+Chatroom.bind('<Return>', send)
+
+
+def receive():
+    global uses
+    while True:
+        data = s.recv(1024)
+        data = data.decode()
+        print(data)
         try:
-            while True:
-                message = conn.recv(1024)            # 发送消息
-                message = message.decode()
-                message = usr_name + ':' + message
-                self.Load(message,addr)
-            conn.close()
-
-
-        # 如果用户断开连接，将该用户从用户列表中删除，然后更新用户列表。
+            uses = json.loads(data)
+            listbox1.delete(0, tkinter.END)
+            listbox1.insert(tkinter.END, "当前在线用户")
+            listbox1.insert(tkinter.END, "------Group chat-------")
+            for x in range(len(uses)):
+                listbox1.insert(tkinter.END, uses[x])
+        # users.append('------Group chat-------')
         except:
-            j = 0            # 用户断开连接
-            for man in users:
-                if man[0] == user:
-                    users.pop(j)       # 服务器段删除退出的用户
-                    break
-                j = j+1
-            USERS = onlines()
-            self.Load(USERS,addr)
-            conn.close()
+            data = data.split('~')
+            message = data[0]
+            userName = data[1]
+            message = '\n' + message
 
-# 将地址与数据（需发送给客户端）存入messages队列。
-    def Load(self, data, addr):
-        lock.acquire()
-        try:
-            messages.put((addr, data))
+            listbox.insert(tkinter.END, message)
 
-        finally:
-            lock.release()
+            # listbox.tag_config('tag2', foreground='red')
+            # listbox.insert(tkinter.END, message, 'tag2')
+            #
+            # listbox.tag_config('tag3', foreground='green')
+            # listbox.insert(tkinter.END, message, 'tag3')
 
-    # 服务端在接受到数据后，会对其进行一些处理然后发送给客户端，如下图，对于聊天内容，服务端直接发送给客户端，而对于用户列表，便由json.dumps处理后发送。
-    def sendData(self): # 发送数据
-        while  True:
-            if not messages.empty():
-                message = messages.get()
-                if isinstance(message[1], str):
-                    for i in range(len(users)):
-                        data = ' ' + message[1]
-                        users[i][1].send(data.encode())
-                        print(data)
-                        print('\n')
+            listbox.see(tkinter.END)
 
-                if isinstance(message[1], list):
-                    data = json.dumps(message[1])
-                    for i in range(len(users)):
-                        try:
-                            users[i][1].send(data.encode())
-                        except:
-                            pass
 
-    def run(self):
-        self.s.bind((Server_IP,PORT))
-        self.s.listen(5)
-        q = threading.Thread(target=self.sendData)
-        q.start()
-        while True:
-            conn, addr = self.s.accept()
-            t = threading.Thread(target=self.receive, args=(conn, addr))
-            t.start()
-        self.s.close()
-if __name__ == '__main__':
-    cserver = ChatServer()
-cserver.start()
+r = threading.Thread(target=receive)
+r.start()  # 开始线程接收信息
+Chatroom.mainloop()
+
+
+
+
+
+
+
+
+
